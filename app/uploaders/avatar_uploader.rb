85d2d1ad16aa115360dd266bbb92ac8fe41586e4
LVALL <valeradracenko418@gmail.com>
2020-12-28 18:28:28 +0200

    change comments

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -35,6 +35,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
   version :thumb do
     process resize_to_fit: [200, 200]
   end
+  version :small do
+    process resize_to_fit: [40, 40]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
